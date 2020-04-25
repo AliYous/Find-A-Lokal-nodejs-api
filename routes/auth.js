fd08f84130535fb9a4c5f62cb7a156da335b6472
ali <aliyoussef34@hotmail.fr>
2020-04-25 18:05:37 +0200

    moved joi validation to a specific file

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require('../model/user');
-
+const { registerValidation } = require('../validation')
 // Validation
 const Joi = require('@hapi/joi')
 const schema = Joi.object({
@@ -13,20 +13,27 @@ const schema = Joi.object({
 router.post('/register', async (req, res) => {
 
     // validate before creating the user
-    const { error } = schema.validate(req.body);
+    const { error } = registerValidation(req.body)
     if(error) return res.status(400).send(error.details[0].message);
+
+    // Checking if user already in the DB
+    const emailExist = await User.findOne({email: req.body.email})
+    if (emailExist) return res.status(400).send('Email already exists')
+     
+    // create new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password 
     });
-
-    // try {
-    //     const savedUser = await user.save();
-    //     res.send(savedUser);
-    // }catch(err) {
-    //     res.status(400).send(err);
-    // }
+    
+    // Save to DB
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err) {
+        res.status(400).send(err);
+    }
 })
 
 // router.post('/login', (req, res) => {
