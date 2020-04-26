8691d831bc3a201afd880f2da21244d870b3be0b
ali <aliyoussef34@hotmail.fr>
2020-04-26 18:32:21 +0200

    added /localcity and /local_id endpoints

--- routes/locals.js
+++ routes/locals.js
@@ -2,12 +2,23 @@ const router = require('express').Router();
 const verify = require('./verifyToken');
 const Local = require('../model/local');
 
-// Show all Locals
+// Get all Locals
 router.get('/', async (req, res) => {
     const locals = await Local.find();
     res.json(locals);
 });
 
-// Update a local
+// Get locals by city
+router.get('/:localCity', async (req, res) => {
+    const locals = await Local.find({ localCity: req.params.localCity})
+    res.json(locals);
+})
+
+// Get one local by id
+router.get('/:local_id', async (req, res) => {
+    const locals = await Local.findById(req.params._id)
+    res.json(locals);
+})
+
 
 module.exports = router
